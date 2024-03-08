22e4ac370aee84942f47a053ed618edb6e171a9b
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-03-08 15:32:02 +0200

    added typescript

---
+++ index.ts
@@ -0,0 +1,8 @@
+// const cardholderName = document.getElementById("name");
+// let cardholderNameDisplay = document.getElementById("cardholder-name");
+
+// cardholderName?.addEventListener("input", (e) => {
+//   cardholderNameDisplay = e.target.value;
+// });
+
+console.log("hello world");
