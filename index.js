2478783ecd193f1b2e7368971fba7cabec7d39dc
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-04-23 20:30:37 +0200

    complete-state

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cardYear = document.getElementById("numberYear");
 const cardYearDisplay = document.getElementById("exp-date-year");
 const cardCVV = document.getElementById("cvc");
 const cardCVVDisplay = document.getElementById("CVV");
+const confirmBtn = document.getElementById("confirm");
 
 function displayUserInputs(input1, input2) {
   if (input1 && input2) {
@@ -30,6 +31,19 @@ function displayUserInputs(input1, input2) {
     alert("Error, please retry");
   }
 }
+
+confirmBtn.addEventListener("click", () => {
+  document.querySelector("main").innerHTML = `
+  <section class="completed-section">
+  <img class="icon-complete"src="./images/icon-complete.svg">
+  <h2>Thank you!</h2
+  <h3>We've added your card details</h3>
+  <button id="continueBtn" onclick="location.reload()" class="buttons">Continue</button>
+  </section>
+  `;
+});
+
+// document.getElementById("continueBtn").addEventListener("click", )
 
 displayUserInputs(cardholderName, cardholderNameDisplay);
 displayUserInputs(cardNumber, cardNumberDisplay);
