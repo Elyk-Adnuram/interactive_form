5c72ea1a3d7c959d1de73dba97c38f09a8df7787
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-06-20 17:18:23 +0200

    unused files deleted

--- index.ts
+++
@@ -1,8 +0,0 @@
-// const cardholderName = document.getElementById("name");
-// let cardholderNameDisplay = document.getElementById("cardholder-name");
-
-// cardholderName?.addEventListener("input", (e) => {
-//   cardholderNameDisplay = e.target.value;
-// });
-
-console.log("hello world");
